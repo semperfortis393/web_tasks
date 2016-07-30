d6f7f4796896877937d4c0cef6ef27cff111b200
semperfortis393 <353252897@qq.com>
2016-07-31 00:31:10 +0800

    选择了鼠标悬停清除 左右按钮hover时改变样式 图片自制 粗糙

--- F1/F1.js
+++ F1/F1.js
@@ -1,5 +1,4 @@
 var size = $(".pic>li").length;
-
 var i = 0;
 $(".left").click(function(){
   i--;
@@ -17,26 +16,37 @@ $(".right").click(function(){
   }
   $(".container .content").eq(i).fadeIn(300).siblings().fadeOut(300);
   $(".btn-ul li").eq(i).addClass("but").siblings().removeClass("but");
+});
+
+$(".container").hover(function(){
+  clearInterval(timer)
 })
 
 
 $(".btn ul li").hover(function () {
-i = $(this).index();
-//将hover的元素下标赋值给i以便后面使用
-//然后将当前图片用eq索引当前按钮位置，并进入第二张图片
-$(".container .content").eq(i).fadeIn().siblings().fadeOut();
-// 清除样式
-$(this).addClass("but").siblings().removeClass("but");
+  i = $(this).index();
+  //将hover的元素下标赋值给i以便后面使用
+  //然后将当前图片用eq索引当前按钮位置，并进入第二张图片
+  $(".container .content").eq(i).fadeIn().siblings().fadeOut();
+  // 清除样式
+  $(this).addClass("but").siblings().removeClass("but");
 });
 
 // 定时轮播
 function start() {
-i++;
-if (i > 3)
-//如果大于3
+  timer=setInterval(function(){
+  i++;
+  if (i > size-1)
     i = 0;
     //就把i初始化为0(转到第一张图片)
-$(".container .content").eq(i).fadeIn().siblings().fadeOut();
-$(".btn ul li").eq(i).addClass("but").siblings().removeClass("but");
+    $(".container .content").eq(i).fadeIn().siblings().fadeOut();
+    $(".btn ul li").eq(i).addClass("but").siblings().removeClass("but");
+  },3000);
 }
-setInterval("start()", 3000);
+start();
+
+//鼠标悬停的时候停止轮播
+$(".container").hover(function(){
+  clearInterval(timer);
+},start
+);
