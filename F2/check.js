012a0a1d177815a7c2a13caf2bb5376226a154fc
semperfortis393 <353252897@qq.com>
2016-08-09 21:37:14 +0800

    样式上的一些修改

--- F2/check.js
+++ F2/check.js
@@ -3,13 +3,12 @@ $("form").submit(function(){
   // title验证
   var titleLength = $("#title").val().length;
     if(titleLength < 6){
-    var wrongMsg1 = '标题不得少于六位';
-    alert(wrongMsg1);
+    alert('标题不得少于六位');
     return false;
-  }
-  // 全部正确会显示输入正确
-      else {
+    }
+    // 全部正确会显示输入正确
+    else {
         var rightMsg = '输入正确';
         alert(rightMsg);
-      }
+    }
   })
