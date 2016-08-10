605d0f679343fc039c8062a2ea5d05f770e0d742
semperfortis393 <353252897@qq.com>
2016-08-10 18:44:53 +0800

    重写了提示文字的动画 不需要每个都必填

--- F1/F1.js
+++ F1/F1.js
@@ -1,66 +1,65 @@
 var size = $(".pic>li").length;
 var i = 0;
-$(".left").click(function(){
-  i--;
-  if(i == -1){
-  i = size-1;
-  }
-  $(".container .content").eq(i).fadeIn(300).siblings().fadeOut(300);
-  $(".btn-ul li").eq(i).addClass("but").siblings().removeClass("but");
+$(".left").click(function() {
+    i--;
+    if (i == -1) {
+        i = size - 1;
+    }
+    $(".container .content").eq(i).fadeIn(300).siblings().fadeOut(300);
+    $(".btn-ul li").eq(i).addClass("but").siblings().removeClass("but");
 });
 
-$(".right").click(function(){
-  i++;
-  if(i == size){
-    i = 0;
-  }
-  $(".container .content").eq(i).fadeIn(300).siblings().fadeOut(300);
-  $(".btn-ul li").eq(i).addClass("but").siblings().removeClass("but");
+$(".right").click(function() {
+    i++;
+    if (i == size) {
+        i = 0;
+    }
+    $(".container .content").eq(i).fadeIn(300).siblings().fadeOut(300);
+    $(".btn-ul li").eq(i).addClass("but").siblings().removeClass("but");
 });
 
 // 显示隐藏左右按钮
-  $(".container").mouseenter(function(){
+$(".container").mouseenter(function() {
     $(".left").animate({
-      opacity:'1',
+        opacity: '1',
     });
     $(".right").animate({
-      opacity:'1',
+        opacity: '1',
     });
-  });
-  $(".container").mouseleave(function(){
+});
+$(".container").mouseleave(function() {
     $(".left").animate({
-      opacity:'0',
+        opacity: '0',
     });
     $(".right").animate({
-      opacity:'0',
+        opacity: '0',
     });
-  });
+});
 
 
-$(".btn ul li").hover(function () {
-  i = $(this).index();
-  //将hover的元素下标赋值给i以便后面使用
-  //然后将当前图片用eq索引当前按钮位置，并进入第二张图片
-  $(".container .content").eq(i).fadeIn().siblings().fadeOut();
-  // 清除样式
-  $(this).addClass("but").siblings().removeClass("but");
+$(".btn ul li").hover(function() {
+    i = $(this).index();
+    //将hover的元素下标赋值给i以便后面使用
+    //然后将当前图片用eq索引当前按钮位置，并进入第二张图片
+    $(".container .content").eq(i).fadeIn().siblings().fadeOut();
+    // 清除样式
+    $(this).addClass("but").siblings().removeClass("but");
 });
 
 // 定时轮播
 function start() {
-  timer=setInterval(function(){
-  i++;
-  if (i > size-1)
-    i = 0;
-    //就把i初始化为0(转到第一张图片)
-    $(".container .content").eq(i).fadeIn().siblings().fadeOut();
-    $(".btn ul li").eq(i).addClass("but").siblings().removeClass("but");
-  },3000);
+    timer = setInterval(function() {
+        i++;
+        if (i > size - 1)
+            i = 0;
+        //就把i初始化为0(转到第一张图片)
+        $(".container .content").eq(i).fadeIn().siblings().fadeOut();
+        $(".btn ul li").eq(i).addClass("but").siblings().removeClass("but");
+    }, 3000);
 }
 start();
 
 //鼠标悬停的时候停止轮播
-$(".container").hover(function(){
-  clearInterval(timer);
-},start
-);
+$(".container").hover(function() {
+    clearInterval(timer);
+}, start);
--- F2/check.js
+++ F2/check.js
@@ -1,14 +1,64 @@
 // 表单提交时候的验证
-$("form").submit(function(){
-  // title验证
-  var titleLength = $("#title").val().length;
-    if(titleLength < 6){
-    alert('标题不得少于六位');
-    return false;
+$("form").submit(function() {
+    // title验证
+    var titleLength = $("#title").val().length;
+    if (titleLength < 6 || titleLength > 15) {
+      alert('标题字数应在6至15位之间');
+      return false;
     }
     // 全部正确会显示输入正确
     else {
-        var rightMsg = '输入正确';
-        alert(rightMsg);
+      var rightMsg = '输入正确';
+      alert(rightMsg);
     }
   })
+  // label动画
+
+// function myFunction(){
+//   var titleLength = $("#title").val().length;
+//   if (titleLength=="") {
+//     document.getElementById("titlelabel").className = "label-down";
+//   }
+//   else {
+//     return false;
+//   }
+// }
+
+$(document).ready(function() {
+    var titleValue = $("input").val();
+    var desValue = $("#des").val();
+    if (titleValue == "") {
+      $("label").addClass("label-down");
+    }
+    if (desValue == "") {
+      $("label").addClass("label-down");
+    }
+  })
+  // title
+$("#title").focus(function() {
+  $("#titlelabel").removeClass("label-down");
+})
+$("#title").blur(function() {
+    $("#titlelabel").addClass("label-down");
+  })
+  // price
+$("#price").focus(function() {
+  $("#pricelabel").removeClass("label-down");
+})
+$("#price").blur(function() {
+    $("#pricelabel").addClass("label-down");
+  })
+  // location
+$("#location").focus(function() {
+  $("#locationlabel").removeClass("label-down");
+})
+$("#location").blur(function() {
+    $("#locationlabel").addClass("label-down");
+  })
+  // description
+$("#des").focus(function() {
+  $("#deslabel").removeClass("label-down");
+})
+$("#des").blur(function() {
+  $("#deslabel").addClass("label-down");
+})
